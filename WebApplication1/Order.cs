a849ed87e7282c3d2f79366de1e5777c147f99a3
Morgan Philo <morgan@philo.co.nz>
2019-02-17 10:56:46 +1300

    Tidy up some unnecessary code changes

--- WebApplication1/Order.cs
+++ WebApplication1/Order.cs
@@ -1,37 +1,35 @@
-﻿using System;
+﻿using MassTransit;
+using MessageContracts;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-using MassTransit;
-using MessageContracts;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1
 {
-    [Route("api/order")]
-    public class Order :
-        Controller
+  [Route("api/order")]
+  public class Order : Controller
+  {
+    private readonly IRequestClient<SubmitOrder, OrderAccepted> _requestClient;
+
+    public Order(IRequestClient<SubmitOrder, OrderAccepted> requestClient)
     {
-        private readonly IRequestClient<SubmitOrder, OrderAccepted> _requestClient;
-
-        public Order(IRequestClient<SubmitOrder, OrderAccepted> requestClient)
-        {
-            _requestClient = requestClient;
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Submit(string id, CancellationToken cancellationToken)
-        {
-            try
-            {
-                OrderAccepted result = await _requestClient.Request(new {OrderId = id}, cancellationToken);
-
-                return Accepted(result.OrderId);
-            }
-            catch (RequestTimeoutException exception)
-            {
-                return StatusCode((int) HttpStatusCode.RequestTimeout);
-            }
-        }
+      _requestClient = requestClient;
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Submit(string id, CancellationToken cancellationToken)
+    {
+      try
+      {
+        OrderAccepted result = await _requestClient.Request(new { OrderId = id }, cancellationToken);
+
+        return Accepted(result.OrderId);
+      }
+      catch (RequestTimeoutException exception)
+      {
+        return StatusCode((int)HttpStatusCode.RequestTimeout);
+      }
+    }
+  }
 }
